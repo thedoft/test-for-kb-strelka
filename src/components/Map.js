c37f1be0f1398cb3ded4d3e0a18b71dad6607281
thedoft <thedoft@gmail.com>
2021-01-24 00:46:11 +0300

    feat: do map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 import PointList from './PointList';
 
 import * as pointCollection from '../points.json';
 
-export default function Map() {
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlZG9mdCIsImEiOiJja2thMjl0aGwwMG9yMndwaWY4M2ptZHBvIn0.8EEq1Pq2hDrZUuIlupJBdQ';
+
+export default function Map(props) {
   const points = pointCollection.features;
+
+  const mapContainer = useRef();
+
+  const [mapState, setMapState] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 2,
+  });
+
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [mapState.lng, mapState.lat],
+      zoom: mapState.zoom,
+    });
+
+    map.on('move', () => {
+      setMapState({
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+  }, []);
 
   return (
     <div className="map">
       <div className="map__left-column">
         <PointList points={points} />
       </div>
-      <div className="map__right-column">
-        
-      </div>
+      <p style={{display: 'inline-block', backgroundColor: '#fff', position: 'absolute', top: 0, right: '10px', zIndex: 1}}>Longitude: {mapState.lng} | Latitude: {mapState.lat} | Zoom: {mapState.zoom}</p>
+      <div className="map__right-column" ref={mapContainer} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import 'normalize.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './index.css';
+
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
@@ -16,7 +18,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
